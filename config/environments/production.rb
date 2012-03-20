0c1f2d42b26e339a62f84b3f18b30ff20d84f4a8
Frank Roland <meet_thrall@freenet.de>
2012-03-20 20:37:21 +0100

    Blueprint again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,8 +47,8 @@ Worte::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
-  config.assets.precompile += %w( blueprint/screen.css blueprint/print.css )
-  # config.assets.precompile += %w( *.css *.js )
+  #config.assets.precompile += %w( blueprint/screen.css blueprint/print.css blueprint/ie.css)
+  config.assets.precompile += %w(  blueprint/*.css  blueprint/*.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
